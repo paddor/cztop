77e67488b96718503b46d91a90aea65ce15560a2
Patrik Wenger <paddor@gmail.com>
2015-12-01 02:55:18 +0100

    Z85: add documentation

--- lib/cztop/z85.rb
+++ lib/cztop/z85.rb
@@ -1,4 +1,6 @@
 module CZTop
+  # Use this class to encode to and from the Z85 encoding algorithm.
+  # @see http://rfc.zeromq.org/spec:32
   class Z85
     include FFIDelegate
     class Error < RuntimeError; end
@@ -8,20 +10,30 @@ module CZTop
       ffi_delegate.set_mode(:mode_z85)
     end
 
-    def encode(string)
-      raise ArgumentError if string.bytesize % 4 > 0
-      string = string.dup.force_encoding(Encoding::BINARY)
-      ptr = ffi_delegate.encode(string, string.bytesize)
+    # Encodes to Z85.
+    # @param input [String] possibly binary input data
+    # @return [String] Z85 encoded data as ASCII string
+    # @raise [ArgumentError] if input length isn't divisible by 4 with no
+    #   remainder
+    def encode(input)
+      raise ArgumentError if input.bytesize % 4 > 0
+      input = input.dup.force_encoding(Encoding::BINARY)
+      ptr = ffi_delegate.encode(input, input.bytesize)
       raise Error if ptr.null?
       z85 = ptr.read_string
       z85.encode!(Encoding::ASCII)
       return z85
     end
 
-    def decode(string)
-      raise ArgumentError if string.bytesize % 5 > 0
+    # Decodes from Z85.
+    # @param input [String] Z85 encoded data
+    # @return [String] original data as binary string
+    # @raise [ArgumentError] if input length isn't divisible by 5 with no
+    #   remainder
+    def decode(input)
+      raise ArgumentError if input.bytesize % 5 > 0
       FFI::MemoryPointer.new(:size_t) do |size_ptr|
-        buffer_ptr = ffi_delegate.decode(string, size_ptr)
+        buffer_ptr = ffi_delegate.decode(input, size_ptr)
         raise Error if buffer_ptr.null?
         decoded_string = buffer_ptr.read_string_length(_size(size_ptr) - 1)
         return decoded_string

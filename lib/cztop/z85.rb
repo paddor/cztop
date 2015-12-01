e108ad1db1e4dbbcf2aa3014f3da72cceaab6f94
Patrik Wenger <paddor@gmail.com>
2015-12-01 02:48:30 +0100

    complete Z85 specs

--- lib/cztop/z85.rb
+++ lib/cztop/z85.rb
@@ -9,21 +9,20 @@ module CZTop
     end
 
     def encode(string)
-      string = string.dup.force_encoding(Encoding::ASCII_8BIT)
-      string << ("\0" * ((4 - (string.bytesize % 4)) % 4)) # pad
-      warn "padded string before encoding: %p" % string
+      raise ArgumentError if string.bytesize % 4 > 0
+      string = string.dup.force_encoding(Encoding::BINARY)
       ptr = ffi_delegate.encode(string, string.bytesize)
       raise Error if ptr.null?
-      return ptr.read_string
+      z85 = ptr.read_string
+      z85.encode!(Encoding::ASCII)
+      return z85
     end
 
     def decode(string)
-      warn "decoding from string: %p" % string
       raise ArgumentError if string.bytesize % 5 > 0
       FFI::MemoryPointer.new(:size_t) do |size_ptr|
         buffer_ptr = ffi_delegate.decode(string, size_ptr)
         raise Error if buffer_ptr.null?
-        warn "decoded string is %i bytes long" % _size(size_ptr)
         decoded_string = buffer_ptr.read_string_length(_size(size_ptr) - 1)
         return decoded_string
       end
--- spec/z85_spec.rb
+++ spec/z85_spec.rb
@@ -1,13 +1,6 @@
 require_relative 'spec_helper'
 
 describe CZTop::Z85 do
-  # "even" means its length is divisible by 4
-  let(:even_string) { "foo bar bazz" } # 12 bytes
-  let(:odd_string) { "foo bar baz" } # 11 bytes
-  let(:binary_data) do
-    "\0\x13\x24\x10\x12\x13\x14\x00\x00".
-      force_encoding(Encoding::ASCII_8BIT)
-  end
   subject { CZTop::Z85.new }
 
   it "instantiates" do
@@ -18,47 +11,77 @@ describe CZTop::Z85 do
     assert_equal "z85", subject.mode
   end
 
-  it "encodes an odd string" do
-    z85_string = subject.encode(odd_string)
-    assert_equal "w]zPgvQTp1vQTM.", z85_string
-  end
-
-  it "round trips with even string" do
-    z85_string = subject.encode(even_string)
-    assert_equal even_string, subject.decode(z85_string)
-  end
-
-  it "encodes an empty string" do
-    assert_equal "", subject.encode("")
-  end
-
-  describe "#decode" do
-    it "decodes an empty string" do
-      assert_equal "", subject.decode("")
+  describe "#encode" do
+    context "empty data" do
+      it "encodes" do
+        assert_equal "", subject.encode("")
+      end
+      it "returns ASCII encoded string" do
+        assert_equal Encoding::ASCII, subject.encode("").encoding
+      end
     end
 
-    context "input of wrong size" do
-      let(:wrong_z85) { "w]zPgvQTp1vQTO" } # 14 instead of 15 chars
+    context "even data" do
+      # "even" means its length is divisible by 4 with no remainder
+
+      # test data from https://github.com/zeromq/rfc/blob/master/src/spec_32.c
+      let(:input) do
+        [
+          0x8E, 0x0B, 0xDD, 0x69, 0x76, 0x28, 0xB9, 0x1D, 
+          0x8F, 0x24, 0x55, 0x87, 0xEE, 0x95, 0xC5, 0xB0, 
+          0x4D, 0x48, 0x96, 0x3F, 0x79, 0x25, 0x98, 0x77, 
+          0xB4, 0x9C, 0xD9, 0x06, 0x3A, 0xEA, 0xD3, 0xB7  
+        ].map { |i| i.chr }.join
+      end
+      let(:expected_output) { "JTKVSB%%)wK0E.X)V>+}o?pNmC{O&4W4b!Ni{Lh6" }
+
+      it "encodes test data" do
+        assert_equal expected_output, subject.encode(input)
+      end
+
+      it "round trips" do
+        z85 = subject.encode(input)
+        assert_equal input, subject.decode(z85)
+      end
+    end
+
+    context "odd data" do
+      # input length is not divisible by 4 with no remainder
+      let(:data) { "foo bar" } # 7 bytes
+
       it "raises" do
-        assert_raises(ArgumentError) { subject.decode(wrong_z85) }
+        assert_raises(ArgumentError) { subject.encode(data) }
       end
     end
   end
 
+  describe "#decode" do
+    context "empty data" do
+      it "decodes" do
+        assert_equal "", subject.decode("")
+      end
+    end
 
-#  it "decodes as binary data" do
-#    z85_string = subject.encode(even_string)
-#    decoded = subject.decode(z85_string)
-#    assert_equal Encoding::ASCII_8BIT, decoded.encoding
-#  end
+    context "even data" do
+      let(:input) { "HelloWorld" }
+      let(:expected_output) do
+        "\x86\x4F\xD2\x6F\xB5\x59\xF7\x5B".force_encoding Encoding::BINARY
+      end
 
-#  it "round trips with binary data" do
-#    z85_string = subject.encode(binary_data)
-#    assert_equal binary_data, subject.decode(z85_string)
-#  end
+      it "decodes" do
+        assert_equal expected_output, subject.decode(input)
+      end
 
-  it "decodes back to the string"
+      it "returns binary encoded string" do
+        assert_equal Encoding::BINARY, subject.decode(input).encoding
+      end
+    end
 
-  it "decodes back to the binary string"
-
+    context "odd data" do
+      let(:data) { "w]zPgvQTp1vQTO" } # 14 instead of 15 chars
+      it "raises" do
+        assert_raises(ArgumentError) { subject.decode(data) }
+      end
+    end
+  end
 end

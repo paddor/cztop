c6fcd903ddec8faa9bebd084c94be92c4373a7e6
Patrik Wenger <paddor@gmail.com>
2015-12-02 03:23:49 +0100

    implement Beacon

--- lib/cztop/beacon.rb
+++ lib/cztop/beacon.rb
@@ -1,4 +1,80 @@
 module CZTop
   class Beacon
+
+    # function pointer to the `zbeacon()` function
+    ZBEACON_FPTR = ::CZMQ::FFI.ffi_libs.each do |dl|
+      fptr = dl.find_function("zbeacon")
+      break fptr unless fptr.nil?
+    end
+
+    def initialize
+      @actor = Actor.new(ZBEACON_FPTR)
+    end
+
+    VERBOSE_CMD = "VERBOSE".freeze
+
+    # Enable verbose logging of commands and activity.
+    def verbose!
+      ::CZMQ::FFI::Zstr.send(@actor, VERBOSE_CMD)
+    end
+
+    CONFIGURE_PIC = "si".freeze
+    CONFIGURE_CMD = "CONFIGURE".freeze
+
+    # @param port_number [Integer]
+    # @return [String] hostname, which can be used as endpoint for incoming
+    #   connections
+    # @raise if the system doesn't support UDP broadcasts
+    def configure(port_number)
+      # TODO: provide Actor#send_picture (or better name, #sys_send)
+      ::CZMQ::FFI::Zsock.send(@actor, CONFIGURE_PIC, CONFIGURE_CMD, port_number)
+      hostname = ::CZMQ::FFI::Zstr.recv(@actor)
+      raise if hostname.empty?
+      return hostname
+    end
+
+    PUBLISH_PIC = "sbi".freeze
+    PUBLISH_CMD = "PUBLISH".freeze
+    MAX_BEACON_DATA = 255
+
+    # Start broadcasting a beacon.
+    # @param data [String] data to publish
+    # @param interval [Integer] interval in msec
+    # @raise if data is longer than {MAX_BEACON_DATA} bytes
+    def publish(data, interval)
+      raise if data.bytesize > MAX_BEACON_DATA
+      ::CZMQ::FFI::Zsock.send(@actor, PUBLISH_PIC, PUBLISH_CMD, data, data.bytesize, interval)
+    end
+
+    SILENCE_CMD = "SILENCE".freeze
+
+    # Stop broadcasting the beacon.
+    def silence
+      ::CZMQ::FFI::Zstr.sendx(@actor, SILENCE_CMD, nil)
+    end
+
+    SUBSCRIBE_PIC = "sb".freeze
+    SUBSCRIBE_CMD = "SUBSCRIBE".freeze
+
+    # Start listening to beacons from peers.
+    # @param filter [String] do a prefix match on received beacons
+    # @param filter [nil] subscribe to all peer beacons
+    def subscribe(filter = nil)
+      ::CZMQ::FFI::Zsock.send(@actor, SUBSCRIBE_PIC, SUBSCRIBE_CMD, filter, filter.bytesize)
+      # TODO
+    end
+
+    UNSUBSCRIBE_CMD = "UNSUBSCRIBE".freeze
+
+    # Stop listening to other peers.
+    def unsubscribe
+      ::CZMQ::FFI::Zstr.sendx(@actor, UNSUBSCRIBE_CMD, nil)
+    end
+
+    # Receive next beacon from a peer.
+    # @return [Message] 2-frame message with ([ipaddr, data])
+    def receive
+      Message.receive_from(@actor)
+    end
   end
 end
--- spec/beacon_spec.rb
+++ spec/beacon_spec.rb
@@ -1,4 +1,11 @@
 require_relative 'spec_helper'
 
 describe CZTop::Beacon do
+  describe "#verbose!"
+  describe "#configure"
+  describe "#publish"
+  describe "#silence"
+  describe "#subscribe"
+  describe "#unsubscribe"
+  describe "#receive"
 end

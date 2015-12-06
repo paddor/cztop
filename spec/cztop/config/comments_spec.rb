ead01f1b9067033099816f9b05fd2069b43f9b65
Patrik Wenger <paddor@gmail.com>
2015-12-06 04:10:09 +0100

    add (empty) spec files for Message::Frames and Config::Comments

---
+++ spec/cztop/config/comments_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../../spec_helper'
+
+describe CZTop::Config::Comments do
+  # TODO
+end
---
+++ spec/cztop/message/frames_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../../spec_helper'
+
+describe CZTop::Message::Frames do
+  # TODO
+end

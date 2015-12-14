09a2389507e09f94d4be1ead33046480f1c54c10
Patrik Wenger <paddor@gmail.com>
2015-12-14 21:56:14 +0100

    implement 90% of Certificate

--- lib/cztop/certificate.rb
+++ lib/cztop/certificate.rb
@@ -3,43 +3,101 @@ module CZTop
   class Certificate
     include HasFFIDelegate
     extend CZTop::HasFFIDelegate::ClassMethods
+    include ::CZMQ::FFI
 
+    # various errors around {Certificate}s
+    class Error < RuntimeError; end
+
+    # Loads a certificate from a file.
+    # @param filename [String, Pathname, #to_s] path to certificate file
+    # @return [Certificate] the loaded certificate
     def self.load(filename)
+      ptr = Zcert.load(filename.to_s)
+      from_ffi_delegate(ptr)
+    end
+
+    # Creates a new certificate from the given keys.
+    # @param public_key [String] binary public key
+    # @param secret_key [String] binary secret key
+    # @return [Certificate] the fresh certificate
+    def self.new_from(public_key, secret_key)
+      raise Error, "no public key given" unless public_key
+      raise Error, "no secret key given" unless secret_key
+
+      raise Error, "invalid public key size" if public_key.bytesize!=KEY_BYTES
+      raise Error, "invalid secret key size" if secret_key.bytesize!=KEY_BYTES
+
+      ptr = Zcert.new_from(public_key, secret_key)
+      raise Error if ptr.null?
+      from_ffi_delegate(ptr)
+    end
+
+    # Initialize a new in-memory certificate with random keys.
+    def initialize
+      attach_ffi_delegate(Zcert.new)
+    end
+
+    # length of a binary key in bytes
+    KEY_BYTES = 32
+
+    # Returns the public key in binary form.
+    # @return [String] binary public key
+    def public_key
+      ffi_delegate.public_key.read_string(KEY_BYTES)
+    end
+
+    # Returns the secret key in binary form.
+    # @return [String] binary secret key
+    def secret_key
+      ffi_delegate.secret_key.read_string(KEY_BYTES)
+    end
+
+    # Returns the public key in Z85 form.
+    # @return [String] Z85-encoded public key
+    def public_key_txt
+      ffi_delegate.public_txt.read_string.force_encoding(Encoding::ASCII)
+    end
+    # Returns the secret key in Z85 form.
+    # @return [String] Z85-encoded secret key
+    def secret_key_txt
+      ffi_delegate.secret_txt.read_string.force_encoding(Encoding::ASCII)
+    end
+
+    # Get metadata.
+    # @return [String] value for meta key
+    # @return [nil] if metadata key is not set
+    def [](key)
+      ptr = ffi_delegate.meta(key)
+      return nil if ptr.null?
+      ptr.read_string
+    end
+    # Set metadata.
+    # @return [value]
+    def []=(key, value)
+      if value
+        ffi_delegate.set_meta(key, "%s", :string, value)
+      else
+        ffi_delegate.set_meta(key, nil)
+      end
+    end
+
+    # Returns meta keys set.
+    # @return [Array<String>]
+    def meta_keys
       # TODO
     end
 
-    # TODO
-    def self.new_from(public_key, secret_key)
-      Zcert.new_from(public_key, secret_key)
-    end
-
-    # TODO
-    def public_key_bytes
-    end
-
-    # TODO
-    def secret_key_bytes
-    end
-
-    # TODO
-    def public_key_txt
-    end
-    # TODO
-    def secret_key_txt
-    end
-
-    # TODO
-    def meta(key)
-    end
-    # TODO
-    def meta=(key, value)
-    end
-    # TODO
-    def meta_keys
-    end
-
-    # TODO
+    # Save full certificate (public + secret) to files.
+    # @param filename [String] path/filename to public file
+    # @return [void]
+    # @raise [Error] if this fails
+    # @note This will create two files: one of the public key and one for the
+    #   secret key. The secret filename is filename + "_secret".
     def save(filename)
+      # see https://github.com/zeromq/czmq/issues/1244
+      raise Error, "filename can't be empty" if filename.to_s.empty?
+      rc = ffi_delegate.save(filename.to_s)
+      raise Error, "error while saving to file %p" % filename if rc == -1
     end
     # TODO
     def save_public(filename)
@@ -50,11 +108,20 @@ module CZTop
     # TODO
     def apply(zocket)
     end
-    # TODO
+
+    # Duplicates the certificate.
+    # @return [Certificate]
+    # @raise [Error] if this fails
     def dup
+      ptr = ffi_delegate.dup
+      raise Error, "unable to duplicate certificate" if ptr.null?
+      from_ffi_delegate(ptr)
     end
-    # TODO
+
+    # Compares this certificate to another.
+    # @return [Boolean] whether they have the same keys
     def ==(other)
+      ffi_delegate.eq(other.ffi_delegate)
     end
   end
 end
--- spec/cztop/certificate_spec.rb
+++ spec/cztop/certificate_spec.rb
@@ -1,5 +1,221 @@
-require_relative '../spec_helper'
+require_relative 'spec_helper'
+require 'tmpdir'
+require 'pathname'
 
 describe CZTop::Certificate do
   include_examples "has FFI delegate"
+
+  context "with certificate given" do
+    let(:cert) { CZTop::Certificate.new }
+    describe "#initialize" do
+      it "initializes" do
+        cert
+      end
+    end
+
+    describe "#public_key" do
+      let(:key) { cert.public_key }
+      it "returns key" do
+        assert_kind_of String, key
+      end
+      it "is binary" do
+        assert_equal Encoding::BINARY, key.encoding
+      end
+      it "has correct length" do
+        assert_equal 32, key.bytesize
+      end
+    end
+
+    describe "#secret_key" do
+      let(:key) { cert.secret_key }
+      it "returns key" do
+        assert_kind_of String, key
+      end
+      it "is binary" do
+        assert_equal Encoding::BINARY, key.encoding
+      end
+      it "has correct length" do
+        assert_equal 32, key.bytesize
+      end
+    end
+
+    describe "#public_key_txt" do
+      let(:key) { cert.public_key_txt }
+      it "returns key" do
+        assert_kind_of String, key
+      end
+      it "is ASCII" do
+        assert_equal Encoding::ASCII, key.encoding
+      end
+      it "has correct length" do
+        assert_equal 40, key.size
+      end
+      it "is Z85 and correct" do
+        assert_equal cert.public_key, CZTop::Z85.new.decode(key)
+      end
+    end
+
+    describe "#secret_key_txt" do
+      let(:key) { cert.secret_key_txt }
+      it "returns key" do
+        assert_kind_of String, key
+      end
+      it "is ASCII" do
+        assert_equal Encoding::ASCII, key.encoding
+      end
+      it "has correct length" do
+        assert_equal 40, key.size
+      end
+      it "is Z85 and correct" do
+        assert_equal cert.secret_key, CZTop::Z85.new.decode(key)
+      end
+    end
+
+    describe "meta information" do
+      Given(:key) { "foo" }
+      Given(:value) { "bar" }
+      describe "#meta" do
+        context "with existing meta key" do
+          Given { cert[key] = value }
+          Then { cert[key] == value }
+        end
+        context "with non-existing meta key" do
+          Then { cert[key].nil? }
+        end
+      end
+
+      describe "#meta=" do
+        let(:ffi_delegate) { cert.ffi_delegate }
+        context "when setting" do
+          it "sets" do
+            expect(ffi_delegate).to(
+              receive(:set_meta).with(key, String, :string, value))
+            cert[key] = value
+          end
+        end
+        context "when unsetting", skip: true do
+          Given { cert[key] = value }
+          When { cert[key] = nil }
+          Then { cert[key].nil? }
+        end
+        it "does safe format handling" do
+          expect(ffi_delegate).to receive(:set_meta).with(String, "%s", any_args)
+          cert[key] = value
+        end
+      end
+
+      describe "#meta_keys" do
+        context "with meta keys set" do
+          it "returns keys" do
+          end
+        end
+        context "with no meta keys set" do
+          it "returns empty array"
+        end
+      end
+
+      describe "#dup" do
+        When(:duplicate_cert) { cert.dup }
+        Then { cert == duplicate_cert }
+
+        context "with failure" do
+          it "raises" do
+            expect(cert.ffi_delegate).to(
+            receive(:dup).and_return(::FFI::Pointer::NULL))
+            assert_raises(CZTop::Certificate::Error) { cert.dup }
+          end
+        end
+      end
+
+      describe ".new_from" do
+        Given(:public_key) { cert.public_key }
+        Given(:secret_key) { cert.secret_key }
+        When(:new_cert) do
+          CZTop::Certificate.new_from(public_key, secret_key)
+        end
+        Then { cert == new_cert && new_cert == cert }
+        context "with invalid public key size" do
+          Given(:public_key) { "too short" }
+          Then { new_cert == Failure(CZTop::Certificate::Error) }
+        end
+        context "with invalid secret key size" do
+          Given(:secret_key) { "too short" }
+          Then { new_cert == Failure(CZTop::Certificate::Error) }
+        end
+        context "with missing public key" do
+          Given(:public_key) { nil }
+          Then { new_cert == Failure(CZTop::Certificate::Error) }
+        end
+        context "with missing secret key" do
+          Given(:secret_key) { nil }
+          Then { new_cert == Failure(CZTop::Certificate::Error) }
+        end
+      end
+
+      describe "#==" do
+        context "with equal certificate" do
+          Given(:other) { cert.dup }
+          Then { cert  == other }
+          And  { other == cert  }
+        end
+        context "with different certificate" do
+          Given(:other) { CZTop::Certificate.new }
+          Then { cert  != other }
+          And  { other != cert  }
+        end
+      end
+
+      describe "#apply" do
+        it "applies to socket"
+      end
+    end
+
+    describe "serialization" do
+      let(:tmpdir) do
+        Pathname.new(Dir.mktmpdir("zcert_test"))
+      end
+      let(:path) { tmpdir + "zcert.txt" }
+
+      describe "#save" do
+        When(:result) { cert.save(path) }
+        context "with valid path" do
+          Given { !path.exist? }
+          Then { path.exist? }
+        end
+        context "with invalid path" do
+          Given(:path) { "/" }
+          Then { result == Failure(CZTop::Certificate::Error) }
+        end
+        context "with empty path" do
+          Given(:path) { "" }
+          Then { result == Failure(CZTop::Certificate::Error) }
+        end
+      end
+
+      describe ".load" do
+        context "with existing file" do
+          before(:each) { cert.save(path) }
+          let(:loaded_cert) { CZTop::Certificate.load(path) }
+          it "loads the certificate" do
+            assert_kind_of CZTop::Certificate, loaded_cert
+            assert_equal cert, loaded_cert
+          end
+        end
+        context "with non-existing file" do
+          it "raises" do
+            assert_raises do
+              CZTop::Certificate.load("/does/not/exist")
+            end
+          end
+        end
+      end
+
+      describe "#save_public" do
+        it "saves public key"
+      end
+      describe "#save_secret" do
+        it "saves secret key"
+      end
+    end
+  end
 end

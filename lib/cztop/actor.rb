6ca91ba9970887df80982ddbc890a9a72d5d26a4
Patrik Wenger <paddor@gmail.com>
2015-12-22 19:43:12 +0100

    fix actor

--- lib/cztop/actor.rb
+++ lib/cztop/actor.rb
@@ -8,53 +8,72 @@ module CZTop
     include ZsockOptions
     include SendReceiveMethods
     include PolymorphicZsockMethods
+    include ::CZMQ::FFI
 
+    class Error < RuntimeError; end
+
+    # Raised when trying to interact with a terminated actor.
+    class DeadActorError < Error; end
+
+    # Creates a new actor. If no callback is given, it'll create a generic one
+    # which will yield every received command (first frame of an arriving
+    # message).
     # @param callback [FFI::Pointer]
-    # @param args [FFI::Pointer]
     # @overload initialize(&task)
     #   @yieldparam command [String]
-    #   @yieldparam pipe [String]
-    def initialize(callback = nil, args = nil)
-      # TODO
-      # After initialization: call signal(pipe, 0)
-      # React to "$TERM" command with destroy
-
-      @running = true
+    #   @yieldparam pipe [CZMQ::FFI::Zsock]
+    def initialize(callback = nil, &task)
       @callback = callback
-      @callback ||= ::CZMQ::FFI::Zactor.fn do |pipe_delegate, _args|
-        p pipe_delegate
-        pipe = CZTop::Socket::PAIR.from_ffi_delegate(pipe) # for yielding
-        p pipe
-        pipe.signal # signal that we're ready
-        while @running
-          puts "waiting for a command to arrive ..."
-          command = ::CZMQ::FFI::Zstr.recv(pipe_delegate)
-          puts "got command!"
-          p command
-          break if command.null? # interrupted
-          p command.read_string
-          case command.read_string
-          when "$TERM"
-            puts "actor received $TERM"
-            @running = false
-          else
-            if block_given?
-              yield command, pipe
-            else
-              # NOTE: just for testing
-              puts "actor received command: %p" % command
-            end
-          end
+      if !@callback
+        unless @task = task
+          raise ArgumentError, "no task given"
         end
       end
-      attach_ffi_delegate(::CZMQ::FFI::Zactor.new(@callback, args))
+
+      attach_ffi_delegate(Zactor.new(callback(), _args = nil))
+    end
+
+    def callback
+      @callback ||= Zactor.fn do |pipe_delegate, _args|
+
+        pipe = CZTop::Socket::PAIR.from_ffi_delegate(pipe_delegate)
+        pipe.signal # signal that we're ready
+
+        running = true
+        while running
+          command_ptr = Zstr.recv(pipe_delegate)
+          break if command_ptr.null? # interrupted
+          command = command_ptr.read_string
+          case command
+          when "$TERM"
+            running = false
+            # NOTE: No explicit signal needed. A dying actor sends signal 0.
+          else
+            @task.call(command, pipe)
+          end
+
+          pipe_delegate.flush # flush partially read message, if any
+        end
+      end
     end
 
     # Send message to this actor.
+    # @param msg [Object] message to send to the actor, see {Message.coerce}
+    # @return [void]
+    # @raise [DeadActorError] if actor is terminated
     def <<(msg)
-      puts "sending message to actor self"
+      raise DeadActorError if @terminated
       Message.coerce(msg).send_to(self)
-      puts "message sent"
+    end
+
+    # Tells the actor to terminate and waits for it.
+    # @return [void]
+    # @raise [DeadActorError] if actor is already terminated
+    def terminate
+      raise DeadActorError if @terminated
+      self << "$TERM"
+      @terminated = true
+      wait
     end
   end
 end
--- spec/cztop/actor_spec.rb
+++ spec/cztop/actor_spec.rb
@@ -15,14 +15,71 @@ describe CZTop::Actor do
     assert_operator described_class, :<, CZTop::PolymorphicZsockMethods
   end
 
-  it "instanciates" do
-    described_class.new
+  let(:actor) do
+    CZTop::Actor.new do |command, pipe_delegate|
+      received << command
+    end
+  end
+  let(:received) { [] }
+
+  describe "#initialize" do
+    context "with callback given" do
+      it "remembers callback"
+    end
+
+    context "with no FFI callback given" do
+      context "with block given" do
+        it "remembers it as task"
+      end
+      context "with no block given" do
+        it "raises"
+      end
+    end
   end
 
-  let(:actor) { CZTop::Actor.new }
-  let(:cmd) { %w[SHOW] }
-  let(:msg) { CZTop::Message.new(cmd) }
-  it "receives commands" do
-    actor << msg
+  describe "#callback" do
+  end
+
+  describe "#<<" do
+    it "sends commands to actor" do
+      actor << "PRINT"
+      actor << %w[SHOW foo bar]
+      actor << %w[DO foo bar]
+      actor.terminate
+      assert_equal %w[ PRINT SHOW DO ], received
+    end
+
+    context "when actor is dead" do
+      before(:each) { actor.terminate }
+      it "raises DeadActorError" do
+        assert_raises(CZTop::Actor::DeadActorError) do
+          actor << "FOO"
+        end
+      end
+    end
+  end
+
+  describe "#terminate" do
+    context "when actor is alive" do
+      it "tells actor to terminate" do
+        expect(actor).to receive(:<<).with("$TERM").and_call_original
+        actor.terminate
+      end
+
+      it "waits for death signal" do
+        expect(actor).to receive(:wait).and_call_original
+        actor.terminate
+      end
+    end
+
+    context "when actor is dead" do
+      before(:each) { actor.terminate }
+
+      it "raises DeadActorError" do
+        assert_raises(CZTop::Actor::DeadActorError) do
+          actor.terminate
+        end
+      end
+    end
   end
 end

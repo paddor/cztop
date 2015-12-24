c76b4f81e5ebf28502f8dcaa660bd159926e5af0
Patrik Wenger <paddor@gmail.com>
2015-12-24 18:25:53 +0100

    refactor Actor stuff a bit... still buggy at process exit

--- lib/cztop/actor.rb
+++ lib/cztop/actor.rb
@@ -10,70 +10,123 @@ module CZTop
     include PolymorphicZsockMethods
     include ::CZMQ::FFI
 
-    class Error < RuntimeError; end
-
     # Raised when trying to interact with a terminated actor.
-    class DeadActorError < Error; end
+    class DeadActorError < RuntimeError; end
 
     # Creates a new actor. If no callback is given, it'll create a generic one
     # which will yield every received command (first frame of an arriving
     # message).
-    # @param callback [FFI::Pointer]
-    # @overload initialize(&task)
-    #   @yieldparam command [String]
-    #   @yieldparam pipe [CZMQ::FFI::Zsock]
+    # @param callback [FFI::Function]
+    # @yieldparam message [Message]
+    # @yieldparam pipe [Socket::PAIR]
+    # @see #process_messages
     def initialize(callback = nil, &task)
-      @callback = callback
-      if !@callback
-        unless @task = task
-          raise ArgumentError, "no task given"
-        end
-      end
-
-      attach_ffi_delegate(Zactor.new(callback(), _args = nil))
+      @running = true
+      @ptr_mtx = Mutex.new # mutex for zactor_t resource
+      @state_mtx = Mutex.new # mutex for state of this object, like @running
+      # NOTE: retain reference on callback
+      @callback = callback || make_default_callback(task)
+      ffi_delegate = Zactor.new(@callback, _args = nil)
+      attach_ffi_delegate(ffi_delegate)
     end
 
-    def callback
-      @callback ||= Zactor.fn do |pipe_delegate, _args|
-
-        pipe = CZTop::Socket::PAIR.from_ffi_delegate(pipe_delegate)
-        pipe.signal # signal that we're ready
-
-        running = true
-        while running
-          command_ptr = Zstr.recv(pipe_delegate)
-          break if command_ptr.null? # interrupted
-          command = command_ptr.read_string
-          case command
-          when "$TERM"
-            running = false
-            # NOTE: No explicit signal needed. A dying actor sends signal 0.
-          else
-            @task.call(command, pipe)
-          end
-
-          pipe_delegate.flush # flush partially read message, if any
-        end
-      end
-    end
-
-    # Send message to this actor.
-    # @param msg [Object] message to send to the actor, see {Message.coerce}
-    # @return [void]
+    # Same as {SendReceiveMethods#<<}, but raises if actor is terminated.
+    # @param message [Object] message to send to the actor, see {Message.coerce}
+    # @return [self]
     # @raise [DeadActorError] if actor is terminated
-    def <<(msg)
-      raise DeadActorError if @terminated
-      Message.coerce(msg).send_to(self)
+    def <<(message)
+      raise DeadActorError if not @running
+      @ptr_mtx.synchronize do
+        super
+      end
+      self
     end
 
     # Tells the actor to terminate and waits for it.
     # @return [void]
     # @raise [DeadActorError] if actor is already terminated
     def terminate
-      raise DeadActorError if @terminated
-      self << "$TERM"
-      @terminated = true
+      @state_mtx.synchronize do
+        raise DeadActorError if not @running
+        self << "$TERM"
+        @running = false
+      end
       wait
+    end
+
+    # @return [Boolean] whether this actor has been terminated
+    def terminated?
+      @state_mtx.synchronize do
+        !@running
+      end
+    end
+
+    private
+
+    # Waits for a signal from the backend pipe.
+    # @return [void]
+    def wait
+      @ptr_mtx.synchronize do
+        super
+      end
+    end
+
+    # Creates a new general purpose callback. Signals the low-level actor that
+    # it's ready to process messages and then calls {#process_messages}.
+    #
+    # @param task [Proc] the user-defined block which is passed to
+    #   {#process_messages}
+    # @return [FFI::Function] the callback function for the low-level actor
+    # @raise [ArgumentError] if no task is given
+    def make_default_callback(task)
+      raise ArgumentError, "no task given" if not task
+      Zactor.fn do |pipe_delegate, _args|
+        begin
+          @pipe = Socket::PAIR.from_ffi_delegate(pipe_delegate)
+          @pipe.signal # handshake, so zactor_new() returns
+          process_messages(&task)
+        rescue
+          p $!
+          # TODO: store it?
+        ensure
+          @state_mtx.synchronize do
+            @running = false
+          end
+        end
+      end
+    end
+
+    # Successively receive messages that were sent to the actor and
+    # yield them to the given block to process them. The block
+    # also gets access to the pipe (a {Socket::PAIR} socket) to the actor so
+    # it can send back the result of a command, if needed.
+    #
+    # When waiting for a message is interrupted, execution is aborted and the
+    # actor will terminate.
+    #
+    # @yieldparam message [Message] message (e.g. command) received
+    # @yieldparam pipe [Socket::PAIR] pipe to write back something into the actor
+    def process_messages
+      while true
+        begin
+          message = next_message
+        rescue Interrupt
+          break
+        end
+
+        if "$TERM" == message.frames.first.to_s
+          # NOTE: No explicit signal needed. A dying actor sends signal 0.
+          break
+        end
+
+        yield message, @pipe
+      end
+    end
+
+    # Receives the next message even across any interrupts.
+    # @return [Message] the next message
+    def next_message
+      @pipe.receive
     end
   end
 end
--- spec/cztop/actor_spec.rb
+++ spec/cztop/actor_spec.rb
@@ -16,40 +16,149 @@ describe CZTop::Actor do
   end
 
   let(:actor) do
-    CZTop::Actor.new do |command, pipe_delegate|
-      received << command
+    CZTop::Actor.new do |msg, pipe_delegate|
+      received_messages << msg
     end
   end
-  let(:received) { [] }
+  let(:received_messages) { [] }
 
   describe "#initialize" do
-    context "with callback given" do
-      it "remembers callback"
+    let(:ffi_delegate) { double "ffi delegate" }
+    let(:ffi_callback) { double("ffi callback") }
+
+    before(:each) do
+      expect(::CZMQ::FFI::Zactor).to receive(:new).with(ffi_callback, nil)
+        .and_return(ffi_delegate)
+      expect_any_instance_of(CZTop::Actor).to receive(:attach_ffi_delegate)
+        .with(ffi_delegate)
+    end
+
+    context "with FFI callback" do
+      let(:actor) { CZTop::Actor.new(ffi_callback) }
+
+      it "remembers callback" do
+        assert_same ffi_callback, actor.instance_variable_get(:@callback)
+      end
+
+      it "doesn't create another callback" do
+        expect_any_instance_of(CZTop::Actor).not_to receive(:make_default_callback)
+        actor
+      end
     end
 
     context "with no FFI callback given" do
-      context "with block given" do
-        it "remembers it as task"
+      let(:actor) { CZTop::Actor.new {} }
+
+      before(:each) do
+        expect_any_instance_of(CZTop::Actor).to receive(:make_default_callback)
+          .and_return(ffi_callback)
       end
-      context "with no block given" do
-        it "raises"
+
+      it "creates a default callback" do
+        actor
       end
     end
   end
 
-  describe "#callback" do
+  describe "#make_default_callback" do
+    let(:actor) do
+      CZTop::Actor.new do |*args|
+        yielded.replace(args)
+        received_messages << args[0]
+      end
+    end
+    let(:yielded) { [] }
+    let(:received_messages) { [] }
+
+    it "recognizes $TERM" do
+      actor.terminate
+      pass
+    end
+
+    context "with no block" do
+      it "raises" do
+        assert_raises(ArgumentError) { actor.__send__(:make_default_callback) }
+      end
+    end
+
+    context "when interrupted" do
+      it "terminates actor" do
+        expect(actor).to receive(:next_message).and_raise(Interrupt).once
+        actor << "foo"
+        actor.__send__(:wait)
+        assert_operator actor, :terminated?
+      end
+    end
+
+    it "yields message and pipe" do
+      actor << "foo"
+      actor.terminate
+      assert_equal 2, yielded.size
+      assert_kind_of CZTop::Message, yielded[0]
+      assert_kind_of CZTop::Socket::PAIR, yielded[1]
+    end
+  end
+
+  describe "#terminated?" do
+    context "when terminated" do
+      it "returns true" do
+        actor.terminate
+        assert actor.terminated?
+      end
+    end
+    context "when not yet terminated" do
+      it "returns false" do
+        refute actor.terminated?
+        actor.terminate
+      end
+    end
   end
 
   describe "#<<" do
-    it "sends commands to actor" do
-      actor << "PRINT"
-      actor << %w[SHOW foo bar]
-      actor << %w[DO foo bar]
-      actor.terminate
-      assert_equal %w[ PRINT SHOW DO ], received
+
+    let(:actor) do
+      CZTop::Actor.new {}
     end
 
-    context "when actor is dead" do
+    context "with commands" do
+      let(:commands) { %w[ PRINT SHOW DO ] }
+      let(:received_commands) do
+        received_messages.map { |msg| msg.frames.first.to_s }
+      end
+      before(:each) do
+        commands.each { |c| actor << c }
+        actor.terminate
+      end
+      it "sends commands to actor" do
+        assert_equal commands, received_commands
+      end
+    end
+
+    it "returns self" do # so it can be chained
+      assert_same actor, actor << "foo"
+    end
+
+    context "with array" do
+      let(:actor) do
+        CZTop::Actor.new do |msg, pipe|
+          received_messages << msg
+        end
+      end
+      let(:received_messages) { [] }
+      let(:msg) { %w[ SHOW foo bar ] }
+
+      before(:each) do
+        actor << msg
+        actor.terminate
+      end
+
+      it "sends one message" do
+        assert_equal 1, received_messages.size
+        assert_equal msg, received_messages.first.frames.map(&:to_s)
+      end
+    end
+
+    context "with dead actor" do
       before(:each) { actor.terminate }
       it "raises DeadActorError" do
         assert_raises(CZTop::Actor::DeadActorError) do
@@ -72,7 +181,7 @@ describe CZTop::Actor do
       end
     end
 
-    context "when actor is dead" do
+    context "with dead actor" do
       before(:each) { actor.terminate }
 
       it "raises DeadActorError" do

c2eb403b12f31d381e1839dfeb3cd1fe11e1be98
Patrik Wenger <paddor@gmail.com>
2016-01-08 23:01:38 +0100

    implement Monitor (WIP)

--- lib/cztop/monitor.rb
+++ lib/cztop/monitor.rb
@@ -1,6 +1,104 @@
+require "pry"
+
 module CZTop
-  # CZMQ monitor.
+  # CZMQ monitor. Listen for socket events.
+  #
+  # This is implemented using an {Actor}.
+  #
   # @see http://api.zeromq.org/czmq3-0:zmonitor
   class Monitor
+    include ::CZMQ::FFI
+
+    # Used for {Monitor} errors.
+    class Error < RuntimeError; end
+
+    # function pointer to the `zmonitor()` function
+    ZMONITOR_FPTR = ::CZMQ::FFI.ffi_libraries.each do |dl|
+      fptr = dl.find_function("zmonitor")
+      break fptr if fptr
+    end
+    raise LoadError, "couldn't find zmonitor()" if ZMONITOR_FPTR.nil?
+
+    # @param socket [Socket, Actor] the socket or actor to monitor
+    def initialize(socket)
+      @actor = Actor.new(ZMONITOR_FPTR, socket)
+    end
+
+    # @return [Actor] the actor behind this Beacon
+    attr_reader :actor
+
+    # Terminates the beacon.
+    # @return [void]
+    def terminate
+      @actor.terminate
+    end
+
+    VERBOSE = "VERBOSE".freeze
+
+    # Enable verbose logging of commands and activity.
+    # @return [void]
+    def verbose!
+      @actor << VERBOSE
+      @event_handler = nil
+    end
+
+    # @return [Array<String>] types of valid events
+    EVENTS = %w[
+      CONNECTED
+      CONNECT_DELAYED
+      CONNECT_RETRIED
+      LISTENING
+      BIND_FAILED
+      ACCEPTED
+      ACCEPT_FAILED
+      CLOSED
+      CLOSE_FAILED
+      DISCONNECTED
+      MONITOR_STOPPED
+      ALL
+    ]
+
+    # Configure monitor to listen for specific events.
+    # @param events [String] one or more events from {EVENTS}
+    # @return [void]
+    def listen(*events)
+      events.each do |event|
+        EVENTS.include?(event) or
+          raise Error, "invalid event: #{event.inspect}"
+      end
+      @actor << [ "LISTEN", *events ]
+    end
+
+    # Start the monitor. After this, you can read events using {#next}.
+    # @return [void]
+    def start
+      @actor << "START"
+      @actor.wait
+    end
+
+    # Get next event. This blocks until the next event is available.
+    # @example
+    #   socket = CZTop::Socket::ROUTER.new("tcp://127.0.0.1:5050")
+    #   # ... normal stuff with socket
+    #
+    #   # do this in another thread, or using a Poller, so it's possible to
+    #   # interact with the socket and the monitor
+    #   Thread.new do
+    #     monitor = CZTop::Monitor.new(socket)
+    #     monitor.listen "CONNECTED", "DISCONNECTED"
+    #     while event = monitor.read
+    #       case event
+    #       when "CONNECTED"
+    #         puts "a client has connected"
+    #       when "DISCONNECTED"
+    #         puts "a client has disconnected"
+    #       end
+    #     end
+    #   end
+    #
+    # @return [String] one of the events from {EVENTS}
+    def next
+      @actor.receive
+    end
   end
 end
---
+++ spec/cztop/monitor_spec.rb
@@ -0,0 +1,90 @@
+require_relative '../spec_helper'
+
+describe "CZTop::Monitor::ZMONITOR_FPTR" do
+  it "points to a dynamic library symbol" do
+    assert_kind_of FFI::DynamicLibrary::Symbol, CZTop::Monitor::ZMONITOR_FPTR
+  end
+end
+
+describe CZTop::Monitor do
+  subject { CZTop::Monitor.new(rep_socket) }
+  let(:actor) { subject.actor }
+  i = 0
+  let(:endpoint) { "inproc://endpoint_monitor_spec_#{i+=1}" }
+  let(:req_socket) { CZTop::Socket::REQ.new(endpoint) }
+  let(:rep_socket) { CZTop::Socket::REP.new(endpoint) }
+
+  after(:each) do
+    subject.terminate
+  end
+
+  it "initializes and terminates" do
+    subject
+  end
+
+  describe "#initialize" do
+    context "with socket" do
+      it "passes socket" do
+        expect(CZTop::Actor).to receive(:new)
+          .with(CZTop::Monitor::ZMONITOR_FPTR, rep_socket).and_call_original
+        subject
+      end
+    end
+  end
+
+  describe "#listen" do
+    context "with one valid event" do
+      let(:event) { "CONNECTED" }
+      it "tells zmonitor actor" do
+        expect(actor).to receive(:<<).with(["LISTEN", event])
+        subject.listen(event)
+      end
+    end
+    context "with multiple valid events" do
+      let(:events) { %w[ CONNECTED DISCONNECTED ] }
+      it "tells zmonitor actor" do
+        expect(actor).to receive(:<<).with(["LISTEN", *events])
+        subject.listen(*events)
+      end
+    end
+    context "with invalid event" do
+      let(:event) { "FOO" }
+      it "raises" do
+        assert_raises(CZTop::Monitor::Error) do
+          subject.listen(event)
+        end
+      end
+    end
+  end
+
+  describe "#start" do
+    it "tells zmonitor to start" do
+      expect(actor).to receive(:<<).with("START")
+      expect(actor).to receive(:wait).at_least(2) # once for termination
+      subject.start
+    end
+  end
+
+  describe "#next", skip: "WIP" do # FIXME
+    it "gets the next event" do
+      subject.verbose!
+      subject.listen(*%w[ALL])
+      subject.start
+      warn "started"
+      Thread.new do
+        req_socket << "bla"
+        warn "sent to REQ"
+      end
+      #req_socket.disconnect(endpoint)
+      t = Thread.new do
+        while event = subject.next
+          warn "got event: #{event.inspect}"
+          break if event == "DISCONNECTED"
+        end
+      end
+      rep_socket.receive
+      warn "received from REP"
+      t.join
+    end
+  end
+end

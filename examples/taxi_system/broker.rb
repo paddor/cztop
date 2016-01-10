5a8af1f04f29320ad05937a6b15dd95091fd3aec
Patrik Wenger <paddor@gmail.com>
2016-01-10 03:00:43 +0100

    fix example

--- examples/taxi_system/broker.rb
+++ examples/taxi_system/broker.rb
@@ -5,7 +5,7 @@ require_relative '../../lib/cztop'
 endpoint = ENV["BROKER_ADDRESS"]
 broker_cert = CZTop::Certificate.load ENV["BROKER_CERT"] # secret+public
 client_certs = ENV["CLIENT_CERTS"] # /path/to/client_certs/
-workers = Pathname.new(client_certs).children.map(&:to_s)
+drivers = Pathname.new(client_certs).children.map(&:basename).map(&:to_s)
 
 authenticator = CZTop::Authenticator.new
 authenticator.verbose!
@@ -17,16 +17,27 @@ socket.options.router_mandatory = true # raise when message unroutable
 socket.bind(endpoint)
 puts "bound."
 
-# ...
-# TODO: use Monitor to wait for all drivers to connect
-# in the meantime: just start client first.
-sleep 5
+# wait for all drivers to connect
+monitor = CZTop::Monitor.new(socket)
+monitor.listen("ACCEPTED")
+monitor.start
+monitor.actor.options.rcvtimeo = 5000
+remaining = drivers.size
+begin
+  while event = monitor.next
+    remaining -= 1 if event[0] == "ACCEPTED"
+    break if remaining == 0
+  end
+rescue Interrupt
+  abort "drivers didn't connect in time."
+else
+  warn "all drivers connected"
+  sleep 0.1 # give ZAP some time to authenticate them
+end
 
-##
-# assuming all workers have connected by now
-#
-workers.each do |worker|
-  socket.send_to(worker, "do something")
+drivers.each do |driver|
+  warn "sending message to driver: #{driver.inspect}"
+  socket << [ driver, "pick customer up at X" ]
 end
 
 # TODO:

ec6a33d6581c06d66cbece94f879c1deb1cc063e
Patrik Wenger <paddor@gmail.com>
2016-01-10 01:31:27 +0100

    simplify ZsockOptions

--- lib/cztop/zsock_options.rb
+++ lib/cztop/zsock_options.rb
@@ -20,26 +20,27 @@ module CZTop
         @zocket = zocket
       end
 
-      # just to shorten the lines
-      Z = ::CZMQ::FFI::Zsock
+      include CZMQ::FFI
 
       # @return [Integer] the send high water mark
-      def sndhwm() Z.sndhwm(@zocket) end
+      def sndhwm() Zsock.sndhwm(@zocket) end
       # @param value [Integer] the new send high water mark.
-      def sndhwm=(value) Z.set_sndhwm(@zocket, value) end
+      def sndhwm=(value) Zsock.set_sndhwm(@zocket, value) end
       # @return [Integer] the receive high water mark
-      def rcvhwm() Z.rcvhwm(@zocket) end
+      def rcvhwm() Zsock.rcvhwm(@zocket) end
       # @param value [Integer] the new receive high water mark
-      def rcvhwm=(value) Z.set_rcvhwm(@zocket, value) end
+      def rcvhwm=(value) Zsock.set_rcvhwm(@zocket, value) end
 
       # @!group (CURVE) Security
 
       # @return [Boolean] whether this zocket is a CURVE server
-      def curve_server?() Z.curve_server(@zocket) > 0 end
+      def curve_server?() Zsock.curve_server(@zocket) > 0 end
 
       # Make this zocket a CURVE server.
       # @param bool [Boolean]
-      def curve_server=(bool) Z.set_curve_server(@zocket, bool ? 1 : 0) end
+      def curve_server=(bool)
+        Zsock.set_curve_server(@zocket, bool ? 1 : 0)
+      end
 
       # @return [String] Z85 encoded server key set
       # @return [nil] if the current mechanism isn't CURVE or CURVE isn't
@@ -53,7 +54,7 @@ module CZTop
       # @return [String, nil] key, if CURVE is supported and active, or nil
       def curve_key(key_name)
         return nil if mechanism != :curve
-        ptr = Z.__send__(key_name, @zocket)
+        ptr = Zsock.__send__(key_name, @zocket)
         return nil if ptr.null?
         ptr.read_string
       end
@@ -66,10 +67,10 @@ module CZTop
       def curve_serverkey=(key)
         case key.bytesize
         when 40
-          Z.set_curve_serverkey(@zocket, key)
+          Zsock.set_curve_serverkey(@zocket, key)
         when 32
           ptr = ::FFI::MemoryPointer.from_string(key)
-          Z.set_curve_serverkey_bin(@zocket, ptr)
+          Zsock.set_curve_serverkey_bin(@zocket, ptr)
         else
           raise ArgumentError, "invalid server key: %p" % key
         end
@@ -88,7 +89,7 @@ module CZTop
       #   etc
       def mechanism
         #int zsock_mechanism (void *self);
-        code = Z.mechanism(@zocket)
+        code = Zsock.mechanism(@zocket)
         MECHANISMS[code] or
           raise "unknown ZMQ security mechanism code: %i" % code
       end
@@ -111,64 +112,68 @@ module CZTop
       # @see http://rfc.zeromq.org/spec:27
       # @return [String]
       def zap_domain
-        Z.zap_domain(@zocket).read_string
+        Zsock.zap_domain(@zocket).read_string
       end
       # Sets the ZAP domain used for authentication.
       # @param domain [String] the new ZAP domain
       def zap_domain=(domain)
         raise ArgumentError, "domain too long" if domain.bytesize > 254
-        Z.set_zap_domain(@zocket, domain)
+        Zsock.set_zap_domain(@zocket, domain)
       end
 
       # @return [Boolean] whether this zocket is a PLAIN server
-      def plain_server?() Z.plain_server(@zocket) > 0 end
+      def plain_server?() Zsock.plain_server(@zocket) > 0 end
 
       # Make this zocket a PLAIN server.
       # @param bool [Boolean]
-      def plain_server=(bool) Z.set_plain_server(@zocket, bool ? 1 : 0) end
+      def plain_server=(bool)
+        Zsock.set_plain_server(@zocket, bool ? 1 : 0)
+      end
 
       # @return [String] username set for PLAIN mechanism
       # @return [nil] if the current mechanism isn't PLAIN
       def plain_username
         return nil if mechanism != :plain
-        Z.plain_username(@zocket).read_string
+        Zsock.plain_username(@zocket).read_string
       end
       # @param username [String] username for PLAIN mechanism
       def plain_username=(username)
-        Z.set_plain_username(@zocket, username)
+        Zsock.set_plain_username(@zocket, username)
       end
       # @return [String] password set for PLAIN mechanism
       # @return [nil] if the current mechanism isn't PLAIN
       def plain_password
         return nil if mechanism != :plain
-        Z.plain_password(@zocket).read_string
+        Zsock.plain_password(@zocket).read_string
       end
       # @param password [String] password for PLAIN mechanism
       def plain_password=(password)
-        Z.set_plain_password(@zocket, password)
+        Zsock.set_plain_password(@zocket, password)
       end
 
       # @!endgroup
 
       # @return [Integer] the timeout when receiving a message
-      def rcvtimeo() Z.rcvtimeo(@zocket) end
+      def rcvtimeo() Zsock.rcvtimeo(@zocket) end
       # @param timeout [Integer] new timeout
-      def rcvtimeo=(timeout) Z.set_rcvtimeo(@zocket, timeout) end
+      def rcvtimeo=(timeout) Zsock.set_rcvtimeo(@zocket, timeout) end
 
       # @return [Integer] the timeout when sending a message
-      def sndtimeo() Z.sndtimeo(@zocket) end
+      def sndtimeo() Zsock.sndtimeo(@zocket) end
       # @param timeout [Integer] new timeout
-      def sndtimeo=(timeout) Z.set_sndtimeo(@zocket, timeout) end
+      def sndtimeo=(timeout) Zsock.set_sndtimeo(@zocket, timeout) end
 
       # Accept only routable messages on ROUTER sockets. Default is off.
       # @param bool [Boolean] whether to error if a message isn't routable
       #   (either if the that peer isn't connected or its SNDHWM is reached)
-      def router_mandatory=(bool) Z.set_router_mandatory(@zocket, bool ? 1 : 0) end
+      def router_mandatory=(bool)
+        Zsock.set_router_mandatory(@zocket, bool ? 1 : 0)
+      end
 
       # @return [String] current socket identity
-      def identity() Z.identity(@zocket).read_string end
+      def identity() Zsock.identity(@zocket).read_string end
       # @param identity [String] new socket identity
-      def identity=(identity) Z.set_identity(@zocket, identity) end
+      def identity=(identity) Zsock.set_identity(@zocket, identity) end
 
 # TODO: a reasonable subset of these
 #//  Get socket options

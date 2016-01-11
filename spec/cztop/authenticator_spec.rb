b5c095184f44b0da331a0116ac59ad562f02415a
Patrik Wenger <paddor@gmail.com>
2016-01-11 02:24:09 +0100

    simplify specs

--- spec/cztop/authenticator_spec.rb
+++ spec/cztop/authenticator_spec.rb
@@ -31,51 +31,40 @@ describe CZTop::Authenticator do
 
   describe "#allow" do
     let(:addrs) { %w[ 1.1.1.1 2.2.2.2 ] }
-    before(:each) do
-      expect(actor).to receive(:<<).with(["ALLOW", *addrs]).and_call_original
-    end
+    after(:each) { subject.allow *addrs }
     it "whitelists addresses" do
-      subject.allow *addrs
+      expect(actor).to receive(:<<).with(["ALLOW", *addrs]).and_call_original
     end
   end
 
   describe "#deny" do
     let(:addrs) { %w[ 3.3.3.3 4.4.4.4 foobar ] }
-    before(:each) do
+    after(:each) { subject.deny *addrs }
+    it "blacklists addresses" do
       expect(actor).to receive(:<<).with(["DENY", *addrs]).and_call_original
     end
-    it "blacklists addresses" do
-      subject.deny *addrs
-    end
-
   end
 
   describe "#plain" do
     let(:filename) { "/path/to/file" }
-    before(:each) do
-      expect(actor).to receive(:<<).with(["PLAIN", filename]).and_call_original
-    end
+    after(:each) { subject.plain(filename) }
     it "enables PLAIN security" do
-      subject.plain(filename)
+      expect(actor).to receive(:<<).with(["PLAIN", filename]).and_call_original
     end
   end
 
   describe "#curve" do
     let(:directory) { "/path/to/directory" }
-    before(:each) do
-      expect(actor).to receive(:<<).with(["CURVE", directory]).and_call_original
-    end
+    after(:each) { subject.curve(directory) }
     it "enables CURVE security" do
-      subject.curve(directory)
+      expect(actor).to receive(:<<).with(["CURVE", directory]).and_call_original
     end
   end
 
   describe "#gssapi" do
-    before(:each) do
-      expect(actor).to receive(:<<).with("GSSAPI").and_call_original
-    end
+    after(:each) { subject.gssapi }
     it "enables GSSAPI security" do
-      subject.gssapi
+      expect(actor).to receive(:<<).with("GSSAPI").and_call_original
     end
   end
 end

ae21ccad9b3ccb2ae2e66df8f6fe936e7fec2512
Patrik Wenger <paddor@gmail.com>
2016-01-11 02:23:44 +0100

    implement Proxy

--- lib/cztop/proxy.rb
+++ lib/cztop/proxy.rb
@@ -6,12 +6,145 @@ module CZTop
   #
   # @see http://api.zeromq.org/czmq3-0:zproxy
   class Proxy
-    # TODO
-    # VERBOSE
-    # FRONTEND
-    # BACKEND
-    # CAPTURE
-    # PAUSE
-    # RESUME
+    include ::CZMQ::FFI
+
+    # function pointer to the `zmonitor()` function
+    ZPROXY_FPTR = ::CZMQ::FFI.ffi_libraries.each do |dl|
+      fptr = dl.find_function("zproxy")
+      break fptr if fptr
+    end
+    raise LoadError, "couldn't find zproxy()" if ZPROXY_FPTR.nil?
+
+    def initialize
+      @actor = Actor.new(ZPROXY_FPTR)
+    end
+
+    # @return [Actor] the actor behind this Beacon
+    attr_reader :actor
+
+    # Terminates the beacon.
+    # @return [void]
+    def terminate
+      @actor.terminate
+    end
+
+    VERBOSE = "VERBOSE".freeze
+
+    # Enable verbose logging of commands and activity.
+    # @return [void]
+    def verbose!
+      @actor << VERBOSE
+      @actor.wait
+    end
+
+    # Returns a configurator object which you can use to configure the
+    # frontend socket.
+    # @return [Configurator] (memoized) frontend configurator
+    def frontend
+      @frontend ||= Configurator.new(self, :frontend)
+    end
+
+    # Returns a configurator object which you can use to configure the backend
+    # socket.
+    # @return [Configurator] (memoized) backend configurator
+    def backend
+      @backend ||= Configurator.new(self, :backend)
+    end
+
+    # Captures all proxied messages and delivers them to a PULL socket bound
+    # to the specified endpoint.
+    # @note The PULL socket has to be bound before calling this method.
+    # @param endpoint [String] the endpoint to which the PULL socket is bound to
+    # @return [void]
+    def capture(endpoint)
+      @actor << ["CAPTURE", endpoint]
+      @actor.wait
+    end
+
+    # Pauses proxying of any messages.
+    # @note This causes any messages to be queued up and potentialy hit the
+    #   high-water mark on the frontend or backend socket, causing messages to
+    #   be dropped or writing applications to block.
+    # @return [void]
+    def pause
+      @actor << "PAUSE"
+      @actor.wait
+    end
+
+    # Resume proxying of messages.
+    # @note This is only needed after a call to {#pause}, not to start the
+    #   proxy. Proxying starts as soon as the frontend and backend sockets are
+    #   properly attached.
+    # @return [void]
+    def resume
+      @actor << "RESUME"
+      @actor.wait
+    end
+
+    class Configurator
+      # @return [Array<Symbol>] supported socket types
+      SOCKET_TYPES = %i[
+        PAIR PUB SUB REQ REP
+        DEALER ROUTER PULL PUSH
+        XPUB XSUB
+      ]
+
+      # @param proxy [Proxy] the proxy instance
+      # @param side [Symbol] :frontend or :backend
+      def initialize(proxy, side)
+        @proxy = proxy
+        @side = case side
+                when :frontend then "FRONTEND"
+                when :backend then "BACKEND"
+                else raise ArgumentError, "invalid side: #{side.inspect}"
+                end
+      end
+
+      # @return [Proxy] the proxy this {Configurator} works on
+      attr_reader :proxy
+
+      # @return [String] the side, either "FRONTEND" or "BACKEND"
+      attr_reader :side
+
+      # Creates and binds a serverish socket.
+      # @param socket_type [Symbol] one of {SOCKET_TYPES}
+      # @param endpoint [String] endpoint to bind to
+      # @raise [ArgumentError] if the given socket type is invalid
+      # @return [void]
+      def bind(socket_type, endpoint)
+        if !SOCKET_TYPES.include?(socket_type)
+          raise ArgumentError, "invalid socket type: #{socket_type}"
+        end
+        @proxy.actor << [ @side, socket_type.to_s, endpoint ]
+        @proxy.actor.wait
+      end
+
+      # Set ZAP domain for authentication.
+      # @param domain [String] the ZAP domain
+      def domain=(domain)
+        @proxy.actor << [ "DOMAIN", @side, domain ]
+        @proxy.actor.wait
+      end
+
+      # Configure PLAIN authentication on this socket.
+      # @note You'll have to use a {CZTop::Authenticator}.
+      def PLAIN_server!
+        @proxy.actor << [ "PLAIN", @side ]
+        @proxy.actor.wait
+      end
+
+      # Configure CURVE authentication on this socket.
+      # @note You'll have to use a {CZTop::Authenticator}.
+      # @param cert [Certificate] this server's certificate,
+      #   so remote clients are able to authenticate this server
+      def CURVE_server!(cert)
+        public_key = cert.public_key
+        secret_key = cert.secret_key or
+          raise ArgumentError, "no secret key in certificate"
+
+        @proxy.actor << [ "CURVE", @side, public_key, secret_key ]
+        @proxy.actor.wait
+      end
+    end
   end
 end
--- spec/cztop/proxy_spec.rb
+++ spec/cztop/proxy_spec.rb
@@ -1,4 +1,262 @@
 require_relative '../spec_helper'
 
+describe "CZTop::Proxy::ZPROXY_FPTR" do
+  it "points to a dynamic library symbol" do
+    assert_kind_of FFI::DynamicLibrary::Symbol, CZTop::Proxy::ZPROXY_FPTR
+  end
+end
+
 describe CZTop::Proxy do
+  let(:proxy) { CZTop::Proxy.new }
+  let(:actor) { proxy.actor }
+
+  after(:each) do
+    proxy.terminate
+  end
+
+  it "initializes and terminates" do
+    proxy
+  end
+
+  describe "#verbose" do
+    after(:each) { proxy.verbose! }
+    it "sends correct message to actor" do
+      expect(actor).to receive(:<<).with("VERBOSE").and_call_original
+    end
+    it "waits for signal" do
+      # +1 for #terminate
+      expect(actor).to receive(:wait).at_least(2).and_call_original
+    end
+  end
+
+  describe "#frontend" do
+    it "returns configurator" do
+      assert_kind_of CZTop::Proxy::Configurator, proxy.frontend
+    end
+    it "memoizes it" do
+      assert_same proxy.frontend, proxy.frontend
+    end
+  end
+
+  describe "#backend" do
+    it "returns configurator" do
+      assert_kind_of CZTop::Proxy::Configurator, proxy.backend
+    end
+    it "memoize it" do
+      assert_same proxy.backend, proxy.backend
+    end
+  end
+
+  describe "#capture" do
+    i = 0
+    let(:endpoint) { "inproc://proxy_capture_spec_#{i+=1}" }
+    context "with endpoint" do
+      after(:each) { proxy.capture(endpoint) }
+      it "tells zproxy to capture" do
+        expect(actor).to receive(:<<).with(["CAPTURE", endpoint]).and_call_original
+      end
+      it "waits for signal" do
+        # +1 for #terminate
+        expect(actor).to receive(:wait).at_least(2).and_call_original
+      end
+    end
+
+  end
+  describe "#pause" do
+    after(:each) { proxy.pause }
+    it "tells zproxy to pause" do
+      expect(actor).to receive(:<<).with("PAUSE").and_call_original
+    end
+    it "waits for signal" do
+      # +1 for #terminate
+      expect(actor).to receive(:wait).at_least(2).and_call_original
+    end
+  end
+  describe "#resume" do
+    after(:each) { proxy.resume }
+    it "tells zproxy to resume" do
+      expect(actor).to receive(:<<).with("RESUME").and_call_original
+    end
+    it "waits for signal" do
+      # +1 for #terminate
+      expect(actor).to receive(:wait).at_least(2).and_call_original
+    end
+  end
+
+  describe CZTop::Proxy::Configurator do
+    let(:configurator) { CZTop::Proxy::Configurator.new(proxy, side) }
+    let(:side) { :frontend } # default for specs
+
+    describe "#initialize" do
+
+      context "with proxy" do
+        it "assigns proxy" do
+          assert_equal proxy, configurator.proxy
+        end
+      end
+      context "with frontend side argument" do
+        let(:side) { :frontend }
+        it "assigns side" do
+          assert_equal "FRONTEND", configurator.side
+        end
+      end
+      context "with backend side argument" do
+        let(:side) { :backend }
+        it "assigns side" do
+          assert_equal "BACKEND", configurator.side
+        end
+      end
+      context "with wrong side argument" do
+        let(:side) { :foo }
+        it "raises" do
+          assert_raises(ArgumentError) { configurator }
+        end
+      end
+    end
+
+    describe "#proxy" do
+      it "returns proxy" do
+        assert_same proxy, configurator.proxy
+      end
+    end
+    describe "#side" do
+      it "returns string" do
+        # NOTE: functionality already tested in #initialize
+        assert_kind_of String, configurator.side
+      end
+    end
+    describe "#bind" do
+      i = 0
+      let(:endpoint) { "inproc://proxy_bind_spec_#{i+=1}" }
+
+      context "with valid arguments" do
+        before(:each) do
+          # +1 for #terminate
+          expect(actor).to receive(:wait).at_least(2).and_call_original
+        end
+        after(:each) do
+          configurator.bind(socket_type, endpoint)
+        end
+        context "for frontend" do
+          let(:side) { :frontend }
+          let(:socket_type) { :ROUTER }
+          it "configures frontend socket" do
+            expect(actor).to receive(:<<)
+              .with(["FRONTEND", "ROUTER", endpoint]).and_call_original
+          end
+        end
+        context "for backend" do
+          let(:side) { :backend }
+          let(:socket_type) { :DEALER }
+          it "configures backend socket" do
+            expect(actor).to receive(:<<)
+              .with(["BACKEND", "DEALER", endpoint]).and_call_original
+          end
+        end
+      end
+      context "with invalid socket type" do
+        let(:type) { :foo }
+        it "raises" do
+          assert_raises(ArgumentError) { configurator.bind(type, endpoint) }
+        end
+      end
+    end
+    describe "#domain=" do
+      let(:domain) { "foobar" }
+      after(:each) { configurator.domain = domain }
+
+      context "for frontend" do
+        let(:side) { :frontend }
+        it "tells actor the ZAP domain" do
+          expect(actor).to receive(:<<)
+            .with(["DOMAIN", "FRONTEND", domain]).and_call_original
+        end
+        it "waits for signal" do
+          # +1 for #terminate
+          expect(actor).to receive(:wait).at_least(2).and_call_original
+        end
+      end
+
+      context "for backend" do
+        let(:side) { :backend }
+        it "tells actor the ZAP domain" do
+          expect(actor).to receive(:<<)
+            .with(["DOMAIN", "BACKEND", domain]).and_call_original
+        end
+        it "waits for signal" do
+          # +1 for #terminate
+          expect(actor).to receive(:wait).at_least(2).and_call_original
+        end
+      end
+    end
+    describe "#PLAIN!" do
+      after(:each) { configurator.PLAIN_server! }
+
+      context "for frontend" do
+        let(:side) { :frontend }
+        it "tells actor to configure PLAIN" do
+          expect(actor).to receive(:<<)
+            .with(["PLAIN", "FRONTEND"]).and_call_original
+        end
+        it "waits for signal" do
+          # +1 for #terminate
+          expect(actor).to receive(:wait).at_least(2).and_call_original
+        end
+      end
+
+      context "for backend" do
+        let(:side) { :backend }
+        it "tells actor to configure PLAIN" do
+          expect(actor).to receive(:<<)
+            .with(["PLAIN", "BACKEND"]).and_call_original
+        end
+        it "waits for signal" do
+          # +1 for #terminate
+          expect(actor).to receive(:wait).at_least(2).and_call_original
+        end
+      end
+    end
+    describe "#CURVE!" do
+      let(:cert) { CZTop::Certificate.new }
+      let(:public_key) { cert.public_key }
+      let(:secret_key) { cert.secret_key }
+
+      context "with correct arguments" do
+        after(:each) { configurator.CURVE_server!(cert) }
+
+        context "for frontend" do
+          let(:side) { :frontend }
+          it "tells actor to configure CURVE" do
+            expect(actor).to receive(:<<)
+              .with(["CURVE", "FRONTEND", public_key, secret_key]).and_call_original
+          end
+          it "waits for signal" do
+            # +1 for #terminate
+            expect(actor).to receive(:wait).at_least(2).and_call_original
+          end
+        end
+
+        context "for backend" do
+          let(:side) { :backend }
+          it "tells actor to configure CURVE" do
+            expect(actor).to receive(:<<)
+              .with(["CURVE", "BACKEND", public_key, secret_key]).and_call_original
+          end
+          it "waits for signal" do
+            # +1 for #terminate
+            expect(actor).to receive(:wait).at_least(2).and_call_original
+          end
+        end
+      end
+
+      context "with secret key missing" do
+        before(:each) do
+          expect(cert).to receive(:secret_key).and_return(nil)
+        end
+        it "raises" do
+          assert_raises(ArgumentError) { configurator.CURVE_server!(cert) }
+        end
+      end
+    end
+  end
 end

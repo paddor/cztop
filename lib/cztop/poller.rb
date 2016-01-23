0eaad6003a8864c08e314622b5e2af9e097de52c
Patrik Wenger <paddor@gmail.com>
2016-01-23 12:50:36 +0100

    reimplement Poller
    * it can now poll for writing, too
    * and it has #readable and #writable, which return arrays of the
      respective sockets
    * this will be useful for celluloid/celluloid-zmq#56

--- lib/cztop/poller.rb
+++ lib/cztop/poller.rb
@@ -1,113 +1,326 @@
 module CZTop
-  # CZMQ poller, a trivial socket poller. This only supports polling for
-  # reading, and only on (CZMQ) {Socket}s and {Actor}s (well, and "raw" ZMQ
-  # sockets).
+  # A non-trivial socket poller.
   #
-  # @see http://api.zeromq.org/czmq3-0:zpoller
+  # It can poll for reading and writing, and supports getting back an array of
+  # readable/writable sockets after the call to {#wait}. The reason for this
+  # feature is to be able to use it in Celluloid::ZMQ, where in a call to
+  # Celluloid::ZMQ::Reactor#run_once all readable/writable sockets need to be
+  # processed.
+  #
+  # This implementation is NOT based on zpoller. Reasons:
+  #
+  # * zpoller can only poll for reading
+  #
+  # It's also NOT based on `zmq_poller()`. Reasons:
+  #
+  # * zmq_poller() doesn't exist in older versions of ZMQ < 4.2
+  #
+  # Possible future implementation on +zmq_poller()+ might work like this, to
+  # support getting an array of readable/writable sockets:
+  #
+  # * in {#wait}, poll with normal timeout
+  # * then poll again with zero timeout until no more sockets, accumulate
+  #   results
+  #
+  # = Limitations
+  #
+  # This poller can't poll for writing on CLIENT/SERVER sockets.
+  # Implementation could be adapted to support them using
+  # {CZTop::Poller::ZPoller}, at least for reading. But it'd make the code
+  # ugly.
+  #
   class Poller
-    include HasFFIDelegate
-    extend CZTop::HasFFIDelegate::ClassMethods
-    include ::CZMQ::FFI
+    # CZTop's interface to the low-level +zmq_poll()+ function.
+    module ZMQ
 
-    # Initializes the Poller. At least one reader has to be given.
-    # @param reader [Socket, Actor] socket to poll for input
-    # @param readers [Socket, Actor] any additional sockets to poll for input
-    def initialize(reader, *readers)
-      @sockets = {} # to keep references and return same instances
-      ptr = Zpoller.new(reader,
-                        *readers.flat_map {|r| [ :pointer, r ] },
-                        :pointer, nil)
-      attach_ffi_delegate(ptr)
-      remember_socket(reader)
-      readers.each { |r| remember_socket(r) }
+      POLL    = 1
+      POLLIN  = 1
+      POLLOUT = 2
+      POLLERR = 4
+
+      extend ::FFI::Library
+      lib_name = 'libzmq'
+      lib_paths = ['/usr/local/lib', '/opt/local/lib', '/usr/lib64']
+        .map { |path| "#{path}/#{lib_name}.#{::FFI::Platform::LIBSUFFIX}" }
+      ffi_lib lib_paths + [lib_name]
+
+      # Represents a struct of type +zmq_pollitem_t+.
+      class PollItem < FFI::Struct
+        ##
+        # shamelessly taken from https://github.com/mtortonesi/ruby-czmq-ffi
+        #
+
+
+        FD_TYPE = if FFI::Platform::IS_WINDOWS && FFI::Platform::ADDRESS_SIZE == 64
+          # On Windows, zmq.h defines fd as a SOCKET, which is 64 bits on x64.
+          :uint64
+        else
+          :int
+        end
+
+        layout  :socket,  :pointer,
+                :fd,      FD_TYPE,
+                :events,  :short,
+                :revents, :short
+
+        # @return [Boolean] whether the socket is readable
+        def readable?
+          (self[:revents] & POLLIN) > 0
+        end
+
+        # @return [Boolean] whether the socket is writable
+        def writable?
+          (self[:revents] & POLLOUT) > 0
+        end
+      end
+
+      opts = {
+        blocking: true  # only necessary on MRI to deal with the GIL.
+      }
+
+      #ZMQ_EXPORT int  zmq_poll (zmq_pollitem_t *items, int nitems, long timeout);
+      attach_function :poll, :zmq_poll, [:pointer, :int, :long], :int, **opts
     end
 
-    # Adds another reader socket to the poller.
-    # @param reader [Socket, Actor] socket to poll for input
+    # @param readers [Socket, Actor] sockets to poll for input
+    def initialize(*readers)
+      @readers = {}
+      @writers = {}
+      @readable = []
+      @writable = []
+      @rebuild_needed = true
+      readers.each { |r| add_reader(r) }
+    end
+
+    # @return [Array<CZTop::Socket>] registered reader sockets
+    def readers
+      @readers.values
+    end
+
+    # @return [Array<CZTop::Socket>] registered writer sockets
+    def writers
+      @writers.values
+    end
+
+    # Adds a socket to be polled for reading.
+    # @param socket [CZTop::Socket] the socket
     # @return [void]
-    # @raise [SystemCallError] if this fails
-    def add(reader)
-      rc = ffi_delegate.add(reader)
-      raise_zmq_err("unable to add socket %p" % reader) if rc == -1
-      remember_socket(reader)
+    def add_reader(socket)
+      ptr = CZMQ::FFI::Zsock.resolve(socket) # get low-level handle
+      @readers[ptr.to_i] = socket
+      @rebuild_needed = true
     end
 
-    # Removes a reader socket from the poller.
-    # @param reader [Socket, Actor] socket to remove
+    # Removes a previously registered reader socket. Won't raise if you're
+    # trying to remove a socket that's not registered.
+    # @param socket [CZTop::Socket] the socket
     # @return [void]
-    # @raise [ArgumentError] if socket was invalid, e.g. it wasn't registered
-    #   in this poller
-    # @raise [SystemCallError] if this fails for another reason
-    def remove(reader)
-      rc = ffi_delegate.remove(reader)
-      raise_zmq_err("unable to remove socket %p" % reader) if rc == -1
-      forget_socket(reader)
+    def remove_reader(socket)
+      ptr = CZMQ::FFI::Zsock.resolve(socket) # get low-level handle
+      @readers.delete(ptr.to_i) and @rebuild_needed = true
     end
 
-    # Wait and return the first socket that becomes readable.
+    # Adds a socket to be polled for writing.
+    # @param socket [CZTop::Socket] the socket
+    # @return [void]
+    def add_writer(socket)
+      ptr = CZMQ::FFI::Zsock.resolve(socket) # get low-level handle
+      @writers[ptr.to_i] = socket
+      @rebuild_needed = true
+    end
+
+    # Removes a previously registered writer socket. Won't raise if you're
+    # trying to remove a socket that's not registered.
+    # @param socket [CZTop::Socket] the socket
+    # @return [void]
+    def remove_writer(socket)
+      ptr = CZMQ::FFI::Zsock.resolve(socket) # get low-level handle
+      @writers.delete(ptr.to_i) and @rebuild_needed = true
+    end
+
+    # Waits for registered sockets to become readable or writable, depending
+    # on what you're interested in.
+    #
     # @param timeout [Integer] how long to wait in ms, or 0 to avoid blocking,
     #   or -1 to wait indefinitely
-    # @return [Socket, Actor]
+    # @return [Socket, Actor] the first readable socket
     # @return [nil] if the timeout expired or
     # @raise [Interrupt] if the timeout expired or
     def wait(timeout = -1)
-      ptr = ffi_delegate.wait(timeout)
-      if ptr.null?
-        raise Interrupt if ffi_delegate.terminated
-        return nil
+      rebuild if @rebuild_needed
+      @readable = @writable = nil
+
+      num = ZMQ.poll(@items_ptr, @nitems, timeout)
+      HasFFIDelegate.raise_zmq_err if num == -1
+
+      return nil if num == 0
+      return readable[0] if readable.any?
+
+      # TODO: handle CLIENT/SERVER sockets using ZPoller
+#      if threadsafe_sockets.any?
+#        zpoller.wait(0)
+#      end
+    end
+
+    # @return [Array<CZTop::Socket>] readable sockets (memoized)
+    def readable
+      @readable ||= @reader_items.select(&:readable?).map do |item|
+        ptr = item[:socket]
+        @readers[ ptr.to_i ]
       end
-      return socket_by_ptr(ptr)
     end
 
-    # Tells the zpoller to ignore interrupts. By default, {#wait} will return
-    # immediately if it detects an interrupt (when +zsys_interrupted+ is set
-    # to something other than zero). Calling this method will supress this
-    # behavior.
-    # @return [void]
-    def ignore_interrupts
-      ffi_delegate.ignore_interrupts
-    end
-
-    # By default the poller stops if the process receives a SIGINT or SIGTERM
-    # signal. This makes it impossible to shut-down message based architectures
-    # like zactors. This method lets you switch off break handling. The default
-    # nonstop setting is off (false).
-    #
-    # Setting this will cause {#wait} to never raise.
-    #
-    # @param flag [Boolean] whether the poller should run nonstop
-    def nonstop=(flag)
-      ffi_delegate.set_nonstop(flag)
+    # @return [Array<CZTop::Socket>] writable sockets (memoized)
+    def writable
+      @writable ||= @writer_items.select(&:writable?).map do |item|
+        ptr = item[:socket]
+        @writers[ ptr.to_i ]
+      end
     end
 
     private
 
-    # Remembers the socket so a call to {#wait} can return with the exact same
-    # instance of {Socket}, and it also makes sure the socket won't get
-    # GC'd.
-    # @param socket [Socket, Actor] the socket instance to remember
+    # Rebuilds the list of `poll_item_t`.
     # @return [void]
-    def remember_socket(socket)
-      @sockets[socket.to_ptr.to_i] = socket
+    def rebuild
+      @nitems = @readers.size + @writers.size
+      @items_ptr = FFI::MemoryPointer.new(ZMQ::PollItem, @nitems)
+      @items_ptr.autorelease = true
+
+      # memory addresses
+      mem = Enumerator.new do |y|
+        @nitems.times { |i| y << @items_ptr + i * ZMQ::PollItem.size }
+      end
+
+      @reader_items = @readers.map{|_,s| new_item(mem.next, s, ZMQ::POLLIN) }
+      @writer_items = @writers.map{|_,s| new_item(mem.next, s, ZMQ::POLLOUT) }
+
+      @rebuild_needed = false
     end
 
-    # Forgets the socket because it has been removed from the poller.
-    # @param socket [Socket, Actor] the socket instance to forget
-    # @return [void]
-    def forget_socket(socket)
-      @sockets.delete(socket.to_ptr.to_i)
+    # @param address [FFI::Pointer] allocated memory address for this item
+    # @param socket [CZTop::Socket] socket we're interested in
+    # @param events [Integer] the events we're interested in
+    # @return [ZMQ::PollItem] a new item for
+    def new_item(address, socket, events)
+      item = ZMQ::PollItem.new(address)
+      item[:socket] = CZMQ::FFI::Zsock.resolve(socket)
+      item[:fd] = 0
+      item[:events] = events
+      item[:revents] = 0
+      item
     end
 
-    # Gets the previously remembered socket associated to the given pointer.
-    # @param ptr [FFI::Pointer] the pointer to a socket
-    # @return [Socket, Actor] the socket associated to the given pointer
-    # @raise [SystemCallError] if no socket is registered under given pointer
-    def socket_by_ptr(ptr)
-      @sockets[ptr.to_i] or
-        # NOTE: This should never happen, since #wait will return nil if
-        # +zpoller_wait+ returned NULL. But it's better to fail early in case
-        # it ever returns a wrong pointer.
-        raise_zmq_err("no socket known for pointer #{ptr.inspect}")
+    # This is the trivial poller based on zpoller. It only supports polling
+    # for reading, but it also supports doing that on CLIENT/SERVER sockets,
+    # which is useful for {CZTop::Poller}.
+    #
+    # @see http://api.zeromq.org/czmq3-0:zpoller
+    class ZPoller
+      include HasFFIDelegate
+      extend CZTop::HasFFIDelegate::ClassMethods
+      include ::CZMQ::FFI
+
+      # Initializes the Poller. At least one reader has to be given.
+      # @param reader [Socket, Actor] socket to poll for input
+      # @param readers [Socket, Actor] any additional sockets to poll for input
+      def initialize(reader, *readers)
+        @sockets = {} # to keep references and return same instances
+        ptr = Zpoller.new(reader,
+                          *readers.flat_map {|r| [ :pointer, r ] },
+                          :pointer, nil)
+        attach_ffi_delegate(ptr)
+        remember_socket(reader)
+        readers.each { |r| remember_socket(r) }
+      end
+
+      # Adds another reader socket to the poller.
+      # @param reader [Socket, Actor] socket to poll for input
+      # @return [void]
+      # @raise [SystemCallError] if this fails
+      def add(reader)
+        rc = ffi_delegate.add(reader)
+        raise_zmq_err("unable to add socket %p" % reader) if rc == -1
+        remember_socket(reader)
+      end
+
+      # Removes a reader socket from the poller.
+      # @param reader [Socket, Actor] socket to remove
+      # @return [void]
+      # @raise [ArgumentError] if socket was invalid, e.g. it wasn't registered
+      #   in this poller
+      # @raise [SystemCallError] if this fails for another reason
+      def remove(reader)
+        rc = ffi_delegate.remove(reader)
+        raise_zmq_err("unable to remove socket %p" % reader) if rc == -1
+        forget_socket(reader)
+      end
+
+      # Wait and return the first socket that becomes readable.
+      # @param timeout [Integer] how long to wait in ms, or 0 to avoid blocking,
+      #   or -1 to wait indefinitely
+      # @return [Socket, Actor]
+      # @return [nil] if the timeout expired or
+      # @raise [Interrupt] if the timeout expired or
+      def wait(timeout = -1)
+        ptr = ffi_delegate.wait(timeout)
+        if ptr.null?
+          raise Interrupt if ffi_delegate.terminated
+          return nil
+        end
+        return socket_by_ptr(ptr)
+      end
+
+      # Tells the zpoller to ignore interrupts. By default, {#wait} will return
+      # immediately if it detects an interrupt (when +zsys_interrupted+ is set
+      # to something other than zero). Calling this method will supress this
+      # behavior.
+      # @return [void]
+      def ignore_interrupts
+        ffi_delegate.ignore_interrupts
+      end
+
+      # By default the poller stops if the process receives a SIGINT or SIGTERM
+      # signal. This makes it impossible to shut-down message based architectures
+      # like zactors. This method lets you switch off break handling. The default
+      # nonstop setting is off (false).
+      #
+      # Setting this will cause {#wait} to never raise.
+      #
+      # @param flag [Boolean] whether the poller should run nonstop
+      def nonstop=(flag)
+        ffi_delegate.set_nonstop(flag)
+      end
+
+      private
+
+      # Remembers the socket so a call to {#wait} can return with the exact same
+      # instance of {Socket}, and it also makes sure the socket won't get
+      # GC'd.
+      # @param socket [Socket, Actor] the socket instance to remember
+      # @return [void]
+      def remember_socket(socket)
+        @sockets[socket.to_ptr.to_i] = socket
+      end
+
+      # Forgets the socket because it has been removed from the poller.
+      # @param socket [Socket, Actor] the socket instance to forget
+      # @return [void]
+      def forget_socket(socket)
+        @sockets.delete(socket.to_ptr.to_i)
+      end
+
+      # Gets the previously remembered socket associated to the given pointer.
+      # @param ptr [FFI::Pointer] the pointer to a socket
+      # @return [Socket, Actor] the socket associated to the given pointer
+      # @raise [SystemCallError] if no socket is registered under given pointer
+      def socket_by_ptr(ptr)
+        @sockets[ptr.to_i] or
+          # NOTE: This should never happen, since #wait will return nil if
+          # +zpoller_wait+ returned NULL. But it's better to fail early in case
+          # it ever returns a wrong pointer.
+          raise_zmq_err("no socket known for pointer #{ptr.inspect}")
+      end
     end
   end
 end
--- spec/cztop/poller_spec.rb
+++ spec/cztop/poller_spec.rb
@@ -2,15 +2,331 @@ require_relative 'spec_helper'
 require 'benchmark'
 
 describe CZTop::Poller do
+  let(:poller) { CZTop::Poller.new }
+
+  i = 0
+  let(:endpoint1) { "inproc://poller_spec_rw1_#{i += 1}" }
+  let(:endpoint2) { "inproc://poller_spec_rw2_#{i += 1}" }
+  let(:endpoint3) { "inproc://poller_spec_rw3_#{i += 1}" }
+
+  let(:reader1) { CZTop::Socket::PULL.new(endpoint1) }
+  let(:reader2) { CZTop::Socket::PULL.new(endpoint2) }
+  let(:reader3) { CZTop::Socket::PULL.new(endpoint3) }
+
+  let(:writer1) { CZTop::Socket::PUSH.new(endpoint1) }
+  let(:writer2) { CZTop::Socket::PUSH.new(endpoint2) }
+  let(:writer3) { CZTop::Socket::PUSH.new(endpoint3) }
+
+  describe "#initialize" do
+    context "with no readers" do
+      it "initializes empty" do
+        assert_empty poller.readers
+        assert_empty poller.writers
+      end
+
+      it "schedules rebuild" do
+        assert poller.instance_variable_get(:@rebuild_needed)
+      end
+    end
+    context "with one reader" do
+      let(:poller) { CZTop::Poller.new(reader1) }
+      it "adds reader" do
+        assert_includes poller.readers, reader1
+      end
+    end
+    context "with multiple readers" do
+      let(:poller) { CZTop::Poller.new(reader1, reader2) }
+      it "adds readers" do
+        assert_includes poller.readers, reader1
+        assert_includes poller.readers, reader2
+      end
+    end
+  end
+  describe "#readers" do
+    it "returns array" do
+      assert_kind_of Array, poller.readers
+    end
+  end
+  describe "#writers" do
+    it "returns array" do
+      assert_kind_of Array, poller.writers
+    end
+  end
+  describe "#add_reader" do
+    before(:each) { poller.add_reader(reader1) }
+    it "adds reader socket" do
+      assert_includes poller.readers, reader1
+    end
+    it "schedules rebuild" do
+      poller.wait(0) # ensure that no rebuild is scheduled
+      refute poller.instance_variable_get(:@rebuild_needed)
+      poller.add_reader(reader2)
+      assert poller.instance_variable_get(:@rebuild_needed)
+    end
+  end
+  describe "#remove_reader" do
+    context "with registered reader" do
+      before(:each) do
+        poller.add_reader(reader1)
+        poller.wait(0) # ensure that no rebuild is scheduled
+        poller.remove_reader(reader1)
+      end
+      it "removes reader" do
+        refute_includes poller.readers, reader1
+      end
+      it "schedules rebuild" do
+        assert poller.instance_variable_get(:@rebuild_needed)
+      end
+    end
+    context "with unregistered reader" do
+      before(:each) do
+        poller.wait(0) # ensure that no rebuild is scheduled
+        poller.remove_reader(reader1)
+      end
+      it "doesn't raise" do end
+      it "doesn't schedule rebuild" do
+        refute poller.instance_variable_get(:@rebuild_needed)
+      end
+    end
+  end
+  describe "#add_writer" do
+    before(:each) { poller.add_writer(writer1) }
+    it "adds writer socket" do
+      assert_includes poller.writers, writer1
+    end
+    it "schedules rebuild" do
+      poller.wait(0) # ensure that no rebuild is scheduled
+      refute poller.instance_variable_get(:@rebuild_needed)
+      poller.add_writer(writer2)
+      assert poller.instance_variable_get(:@rebuild_needed)
+    end
+  end
+  describe "#remove_writer" do
+    context "with registered writer" do
+      before(:each) do
+        poller.add_writer(writer1)
+        poller.wait(0) # ensure that no rebuild is scheduled
+        poller.remove_writer(writer1)
+      end
+      it "removes writer" do
+        refute_includes poller.writers, writer1
+      end
+      it "schedules rebuild" do
+        assert poller.instance_variable_get(:@rebuild_needed)
+      end
+    end
+    context "with unregistered writer" do
+      before(:each) do
+        poller.wait(0) # ensure that no rebuild is scheduled
+        poller.remove_writer(writer1)
+      end
+      it "doesn't raise" do end
+      it "doesn't schedule rebuild" do
+        refute poller.instance_variable_get(:@rebuild_needed)
+      end
+    end
+  end
+
+  describe "#wait" do
+    context "with no registered sockets" do
+      it "doesn't raise" do
+        poller.wait(0)
+      end
+    end
+
+    context "having called previously" do
+      before(:each) do
+        writer1 << "i'll teach you how to read"
+        poller.add_reader(reader1)
+        poller.add_writer(writer1)
+        poller.wait(20)
+        assert_includes poller.readable, reader1
+        readers_before # remember current object
+        writers_before # remember current object
+      end
+      let(:readers_before) { poller.readable }
+      let(:writers_before) { poller.writable }
+      it "forgets readable and writable sockets" do
+        reader1.receive # empty it
+        poller.wait(0)
+        refute_same readers_before, poller.readable
+        refute_same writers_before, poller.writers
+      end
+      it "is level-triggered" do # recognizes the socket as readable again
+        poller.wait(0)
+        assert_includes poller.readable, reader1
+      end
+    end
+    context "when rebuild is not needed" do
+      before(:each) do
+        poller.add_reader(reader1)
+        poller.wait(0)
+      end
+      it "doesn't rebuild" do
+        expect(poller).not_to receive(:rebuild)
+      end
+      after(:each) { poller.wait(0) }
+    end
+    context "when rebuild is needed" do
+      before(:each) do
+        poller.add_reader(reader1)
+        poller.wait(0)
+        poller.add_reader(reader2)
+      end
+      it "rebuilds" do
+        expect(poller).to receive(:rebuild).and_call_original
+      end
+      after(:each) { poller.wait(0) }
+    end
+
+    context "with timeout" do
+      let(:timeout) { 22 }
+      before(:each) do
+        poller.add_reader(reader1)
+        poller.add_reader(reader2)
+        poller.add_reader(reader3)
+        poller.add_writer(reader1)
+        poller.add_writer(reader2)
+        poller.add_writer(reader3)
+      end
+      it "calls zmq_poll" do
+        expect(CZTop::Poller::ZMQ).to receive(:poll)
+          .with(FFI::Pointer, 6, timeout).and_return(0)
+      end
+      after(:each) { poller.wait(timeout) }
+    end
+
+    context "with readable socket" do
+      before(:each) do
+        writer1 << "foobar"
+        poller.add_reader(reader1)
+      end
+      it "returns first readable socket" do
+        assert_same reader1, poller.wait(20)
+      end
+    end
+    context "with no readable socket" do
+      before(:each) do
+        poller.add_reader(reader1)
+      end
+      it "returns nil" do
+        assert_nil poller.wait(20)
+      end
+    end
+    context "with non-socket pointer" do
+      let(:ptr) { FFI::MemoryPointer.from_string("foobar") } # not a socket
+      before(:each) do
+        poller.add_reader(ptr)
+      end
+      it "raises" do
+        assert_raises(Errno::ENOTSOCK) { poller.wait(0) }
+      end
+    end
+    context "with readable server socket" do
+      let(:server) { CZTop::Socket::SERVER.new(endpoint1) }
+      let(:client) { CZTop::Socket::CLIENT.new(endpoint1) }
+      before(:each) do
+        client << "hello"
+        poller.add_reader(server)
+      end
+      it "raises" do
+        assert_raises(ArgumentError) { poller.wait(0) }
+      end
+    end
+  end
+
+  describe "#readable" do
+    context "with no previous call to #wait" do
+      it "returns empty array" do
+        assert_equal [], poller.readable
+      end
+    end
+    context "with readable and unreadable socket" do
+      before(:each) do
+        writer1 << "foobar"
+        poller.add_reader(reader1) # readable
+        poller.add_reader(reader2) # unreadable
+        poller.add_writer(writer1) # a writer
+        poller.wait(20)
+      end
+      it "returns array" do
+        assert_kind_of Array, poller.readable
+      end
+      it "returns readable socket" do
+        assert_equal [reader1], poller.readable
+      end
+      it "memoizes" do
+        assert_same poller.readable, poller.readable
+      end
+    end
+  end
+  describe "#writable" do
+    context "with no previous call to #wait" do
+      it "returns empty array" do
+        assert_equal [], poller.writable
+      end
+    end
+    context "with writable and unwritable socket" do
+
+      let(:writable) { CZTop::Socket::DEALER.new(endpoint1) }
+      let(:unwritable) do
+        s = CZTop::Socket::DEALER.new # an unconnected socket is not writable
+        s.options.sndtimeo = 10 # don't block forever
+        s
+      end
+      before(:each) do
+        poller.add_writer(writable) # writable
+        poller.add_writer(unwritable) # unwritable
+        poller.add_reader(reader3) # a reader
+        poller.wait(20)
+      end
+      it "returns array" do
+        assert_kind_of Array, poller.writable
+      end
+      it "returns writable socket" do
+        assert_equal [writable], poller.writable
+      end
+      it "memoizes" do
+        assert_same poller.writable, poller.writable
+      end
+    end
+  end
+
+  describe "with actor" do
+    let(:actor) do # echo actor
+      CZTop::Actor.new { |msg, pipe| pipe << msg }
+    end
+
+    before(:each) do
+      poller.add_reader(actor)
+    end
+    after(:each) do
+      actor.terminate
+    end
+
+    context "with unreadable actor" do
+      it "returns nil" do
+        assert_nil poller.wait(20)
+      end
+    end
+    context "with readable actor" do
+      before(:each) { actor << "foo" }
+      it "returns actor" do
+        assert_same actor, poller.wait(20)
+      end
+    end
+  end
+end
+
+describe CZTop::Poller::ZPoller do
   include_examples "has FFI delegate"
 
-  let(:poller) { CZTop::Poller.new(reader1) }
+  let(:poller) { CZTop::Poller::ZPoller.new(reader1) }
   let(:ffi_delegate) { poller.ffi_delegate }
-  i = 57578
-  let(:reader1) { CZTop::Socket::PULL.new("inproc://poller_spec_r1_#{i += 1}") }
-  let(:reader2) { CZTop::Socket::PULL.new("inproc://poller_spec_r2_#{i += 1}") }
-  let(:reader3) { CZTop::Socket::PULL.new("inproc://poller_spec_r3_#{i += 1}") }
-
+  i = 0
+  let(:reader1) { CZTop::Socket::PULL.new("inproc://zpoller_spec_r1_#{i += 1}") }
+  let(:reader2) { CZTop::Socket::PULL.new("inproc://zpoller_spec_r2_#{i += 1}") }
+  let(:reader3) { CZTop::Socket::PULL.new("inproc://zpoller_spec_r3_#{i += 1}") }
 
   describe "#initialize" do
     after(:each) { poller }
@@ -21,24 +337,24 @@ describe CZTop::Poller do
           .with(reader1, :pointer, nil).and_call_original
       end
       it "remembers the reader" do
-        expect_any_instance_of(CZTop::Poller).to receive(:remember_socket)
+        expect_any_instance_of(CZTop::Poller::ZPoller).to receive(:remember_socket)
           .with(reader1)
         poller
       end
     end
     context "with additional readers" do
-      let(:poller) { CZTop::Poller.new(reader1, reader2, reader3) }
+      let(:poller) { CZTop::Poller::ZPoller.new(reader1, reader2, reader3) }
       it "passes all readers" do
         expect(CZMQ::FFI::Zpoller).to receive(:new)
           .with(reader1, :pointer, reader2, :pointer, reader3, :pointer, nil)
           .and_call_original
       end
       it "remembers all readers" do
-        expect_any_instance_of(CZTop::Poller).to receive(:remember_socket)
+        expect_any_instance_of(CZTop::Poller::ZPoller).to receive(:remember_socket)
           .with(reader1)
-        expect_any_instance_of(CZTop::Poller).to receive(:remember_socket)
+        expect_any_instance_of(CZTop::Poller::ZPoller).to receive(:remember_socket)
           .with(reader2)
-        expect_any_instance_of(CZTop::Poller).to receive(:remember_socket)
+        expect_any_instance_of(CZTop::Poller::ZPoller).to receive(:remember_socket)
           .with(reader3)
       end
     end

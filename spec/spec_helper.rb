efc31086e7f18c63571d36d3e1b6f26da3db1b36
Patrik Wenger <paddor@gmail.com>
2016-01-28 18:37:04 +0100

    increase rlimit for number of open FD if it's lower than 1024 (like on OSX), so test suite can pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,11 @@ RSpec.configure do |config|
   config.expect_with :minitest
   include CZMQHelper
 end
+
+# NOTE: as of January 28, 2016, the test suite needs opens about ~650 file
+# descriptors. Some OS (like OSX) has a very low default limit of 256. Let's
+# raise it to a more sane 1024, like on Linux.
+soft_limit, hard_limit = Process.getrlimit(:NOFILE)
+if soft_limit < 1024
+  Process.setrlimit(:NOFILE, 1024, hard_limit)
+end
